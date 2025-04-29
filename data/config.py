8b986435e7a5dbcb0531e68520450f9b3b16496e
abduvohid26 <abdujalilovabduvohid6@gmail.com>
2025-04-29 12:50:31 +0500

    check

--- data/config.py
+++ data/config.py
@@ -1,7 +1,9 @@
-from environs import Env
-env = Env()
-env.read_env()
-BOT_TOKEN=env.str('BOT_TOKEN')
+# from environs import Env
+# env = Env()
+# env.read_env()
+from decouple import config
+# BOT_TOKEN=env.str('BOT_TOKEN')
+BOT_TOKEN=config('BOT_TOKEN')
 # ADMINS=env.list('ADMINS')
 ADMINS = [816660001, 5700964012]
 KINO_CHANNEL = [-1002035781732]
